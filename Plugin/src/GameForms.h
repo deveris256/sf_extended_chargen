b26c84179be4092f51f527b4f49d00a1982e1387
deveris256 <deveris256@users.noreply.github.com>
2024-10-28 14:04:38 +0000

    chore: style formatting

--- Plugin/src/GameForms.h
+++ Plugin/src/GameForms.h
@@ -4,14 +4,14 @@ class BGSAVMData : public RE::TESForm
 {
 public:
 	RE::BSFixedString editorName;  // 30
-	BGSAVMData*   unk38;       // 38 - Points to itself?
+	BGSAVMData*       unk38;       // 38 - Points to itself?
 	enum Type
 	{
 		SIMPLE,
 		COMPLEX,
 		MODULATION
 	};
-	uint64_t           type;   // 40
+	uint64_t          type;   // 40
 	RE::BSFixedString name;   // 48
 	RE::BSFixedString name2;  // 50
 	struct Entry
@@ -22,12 +22,12 @@ public:
 		{
 			uint8_t r, g, b, a;
 		};
-		Color color;
-		uint32_t   unk14;
+		Color    color;
+		uint32_t unk14;
 	};
-	Entry* entryBegin;  // 58
-	Entry* entryEnd;    // 60
-	Entry* unk68;       // 68 - Why are there 2 end pointers?
-	uint64_t    unk70;       // 70
+	Entry*   entryBegin;  // 58
+	Entry*   entryEnd;    // 60
+	Entry*   unk68;       // 68 - Why are there 2 end pointers?
+	uint64_t unk70;       // 70
 };
 static_assert(sizeof(BGSAVMData) == 0x80);
